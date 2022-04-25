8a23c71d5d7966bbcf6ca367c15176e9aa82dad9
Tom-Edgar <37910940+Tom-Edgar@users.noreply.github.com>
2022-04-25 09:06:36 -0700

    Update MultiplicationMod100_4-24-2022.py

--- MultiplicationMod100_4-24-2022.py
+++ MultiplicationMod100_4-24-2022.py
@@ -13,26 +13,25 @@ class MultMod100(Scene):
         n=100
         m=0
         for k in range(2,52):
-            if math.gcd(k,100)!=1 and m<50:
-                tempenv=VGroup()
-                Points=[(3*math.cos(2*PI*i/n), 3*math.sin(2*i*PI/n), 0) for i in range(n)]
-                Circ=ParametricCurve(
-                    lambda u: (3*math.cos(u),3*math.sin(u),0), t_min=0, t_max=2*PI)
-                Circ.set_stroke(WHITE, width=4)
-                tempenv.add(Circ)
-                theLines=VGroup()
-                for i in range(n):
-                    theLines.add(Line(Points[i],Points[(k*i)%n]).set_stroke(width=1))
-                tempenv.add(theLines)
-                DiffDiagrams.add(tempenv)
-                m+=1
+            tempenv=VGroup()
+            Points=[(3*math.cos(2*PI*i/n), 3*math.sin(2*i*PI/n), 0) for i in range(n)]
+            Circ=ParametricCurve(
+                lambda u: (3*math.cos(u),3*math.sin(u),0), t_min=0, t_max=2*PI)
+            Circ.set_stroke(WHITE, width=4)
+            tempenv.add(Circ)
+            theLines=VGroup()
+            for i in range(n):
+                theLines.add(Line(Points[i],Points[(k*i)%n]).set_stroke(width=1))
+            tempenv.add(theLines)
+            DiffDiagrams.add(tempenv)
+            m+=1
 
         DiffDiagrams.scale(.25)
-        Rows=[VGroup(*DiffDiagrams[:8])]
+        Rows=[VGroup(*DiffDiagrams[:10])]
         Rows[0].arrange(RIGHT)
         Rows[0].to_edge(UP, buff=.65)
         for i in range(1,5):
-            temp=VGroup(*DiffDiagrams[i*8:i*8+8])
+            temp=VGroup(*DiffDiagrams[i*10:i*10+10])
             temp.arrange(RIGHT)
             temp.next_to(Rows[i-1],DOWN)
             Rows.append(temp)
@@ -40,25 +39,19 @@ class MultMod100(Scene):
         anis1=[]
         for x in DiffDiagrams:
             anis1.append(ShowCreation(x[0]))
-        self.play(*anis1, run_time=4)
+        self.play(*anis1, run_time=3)
 
         anis2 = []
         for x in DiffDiagrams:
             anis2.append(ShowCreation(x[1]))
         self.play(*anis2, run_time=10)
         self.wait(2)
-
-        i=0
-        thelist=[i for i in range(len(DiffDiagrams))]
-        random.shuffle(thelist)
+        
+        i = 0
+        thelist = [i for i in range(len(DiffDiagrams))]
         for j in thelist:
             self.play(FadeOut(DiffDiagrams[j][1]))
-            DiffDiagrams[j][1].set_stroke(color=colors[thelist[i]%4])
+            DiffDiagrams[j][1].set_stroke(color=colors[thelist[i] % 4])
             self.play(ShowCreation(DiffDiagrams[j][1]), run_time=3)
-            i+=1
+            i += 1
         self.wait(5)
-        
-
-
-
-
